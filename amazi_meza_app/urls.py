e17fb4995da6c87ac7f84d79bc7b20d9a516292d
mbanje <mbanjejvernack@gmail.com>
2017-08-01 15:52:49 +0200

    Progam commune and colline menus

--- amazi_meza_app/urls.py
+++ amazi_meza_app/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'^mapping$', views.mapping, name='mapping'),
     url(r'^finance$', views.finance, name='finance'),
     url(r'external_request', handel_rapidpro_request, name="handel_request"),
+    url(r'^getcollinesincommune', views.getCollinesInCommune, name='getcollinesincommune'),
+    url(r'^getwanteddata', views.getwanteddata, name='getwanteddata'),
 ]
--- amazi_meza_app/views.py
+++ amazi_meza_app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+import json
+from amazi_meza_app.models import *
+from django.core import serializers
+from django.http import HttpResponse
 
 def landing(request):
     d = {}
@@ -12,8 +16,9 @@ def home(request):
 
 def problems(request):
     d = {}
+    d["pagetitle"] = "Problems"
+    d["communes"] = Commune.objects.all()
     return render(request, 'problems.html', d)
-
 
 def mapping(request):
     d = {}
@@ -22,3 +27,46 @@ def mapping(request):
 def finance(request):
     d = {}
     return render(request, 'finance.html', d)
+
+def getCollinesInCommune(request):
+    response_data = {}
+    if request.method == 'POST':
+        # import pdb; pdb.set_trace()
+        json_data = json.loads(request.body)
+        code = json_data['code']
+        if (code):
+            collines = Colline.objects.filter(commune=Commune.objects.get(code=code))
+            response_data = serializers.serialize('json', collines)
+        return HttpResponse(response_data, content_type="application/json")
+
+
+
+def getwanteddata(request):
+    response_data = {}
+    if request.method == 'POST':
+        #import pdb; pdb.set_trace()
+        json_data = json.loads(request.body)
+        level = json_data['level']
+        code = json_data['code']
+        start_date = json_data['start_date']
+        end_date = json_data['end_date']
+        wp_pb_reports = ""
+        all_data = []
+
+        if (level):
+            colline_list = None
+            if (level == "colline"):
+                colline_list = Colline.objects.filter(code = code)
+
+            elif (level == "commune"):
+                commune_list = Commune.objects.filter(code = code)
+                if (commune_list):
+                    colline_list = Colline.objects.filter(commune__in = commune_list)
+            elif (level == "national"):
+                colline_list = Colline.objects.all()
+
+            if (colline_list):
+                wp_pb_reports = WaterPointProblem.objects.all()
+
+        response_data = serializers.serialize('json', wp_pb_reports)
+        return HttpResponse(response_data, content_type="application/json")
