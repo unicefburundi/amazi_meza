98ca34992ec6b0eb8f72eae884b50883b95b62e0
RUGANO Allan Stockman <kuristockyrugano@gmail.com>
2017-10-16 13:08:41 +0200

    added import-export

--- amazi_meza_app/admin.py
+++ amazi_meza_app/admin.py
@@ -1,26 +1,98 @@
 from django.contrib import admin
 from amazi_meza_app.models import *
+from import_export.admin import ImportExportModelAdmin
 
 
-
-admin.site.register(LocalLevelReporter)
-admin.site.register(NumberOfWaterPointCommittee)
-admin.site.register(NumberOfHouseHold)
-admin.site.register(WaterNetworkProblemType)
-admin.site.register(NumberOfWaterNetworkProblems)
-admin.site.register(MonthlyIncome)
-admin.site.register(ExpenditureCategory)
-admin.site.register(MonthlyExpenditure)
-admin.site.register(ExpectedBudgetExpenditureAndAnnualBudget)
-admin.site.register(WaterPointProblemTypes)
-admin.site.register(WaterPointType)
-admin.site.register(NumberOfWaterSourceEndPoint)
-admin.site.register(CommuneLevelReporters)
-admin.site.register(WaterNetWork)
-admin.site.register(WaterSourceEndPoint)
-admin.site.register(ActionsForWaterPointProblem)
-admin.site.register(WaterPointProblem)
-admin.site.register(TemporaryLocalLevelReporter)
-admin.site.register(TemporaryCommuneLevelReporters)
+@admin.register(LocalLevelReporter)
+class LocalLevelReporterAdmin(ImportExportModelAdmin):
+    pass
 
 
+@admin.register(NumberOfWaterPointCommittee)
+class NumberOfWaterPointCommitteeAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(NumberOfHouseHold)
+class NumberOfHouseHoldAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(WaterNetworkProblemType)
+class WaterNetworkProblemTypeAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(NumberOfWaterNetworkProblems)
+class NumberOfWaterNetworkProblemsAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(MonthlyIncome)
+class MonthlyIncomeAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(ExpenditureCategory)
+class ExpenditureCategoryAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(MonthlyExpenditure)
+class MonthlyExpenditureAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(ExpectedBudgetExpenditureAndAnnualBudget)
+class TemporaryAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(WaterPointProblemTypes)
+class ExpectedBudgetExpenditureAndAnnualBudgetAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(WaterPointType)
+class WaterPointTypeAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(NumberOfWaterSourceEndPoint)
+class NumberOfWaterSourceEndPointAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(CommuneLevelReporters)
+class CommuneLevelReportersAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(WaterNetWork)
+class WaterNetWorkAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(WaterSourceEndPoint)
+class WaterSourceEndPointAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(ActionsForWaterPointProblem)
+class ActionsForWaterPointProblemAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(WaterPointProblem)
+class WaterPointProblemAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(TemporaryLocalLevelReporter)
+class TemporaryLocalLevelReporterAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(TemporaryCommuneLevelReporters)
+class TemporaryCommuneLevelReportersAdmin(ImportExportModelAdmin):
+    pass
--- public_administration_structure_app/admin.py
+++ public_administration_structure_app/admin.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from public_administration_structure_app.models import *
+from import_export.admin import ImportExportModelAdmin
 
-admin.site.register(Province)
-admin.site.register(Commune)
-admin.site.register(Colline)
-admin.site.register(SousColline)
+
+@admin.register(Province)
+class ProvinceAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(Commune)
+class CommuneAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(Colline)
+class CollineAdmin(ImportExportModelAdmin):
+    pass
+
+
+@admin.register(SousColline)
+class SousCollineAdmin(ImportExportModelAdmin):
+    pass
